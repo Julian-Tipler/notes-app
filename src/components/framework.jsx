2cdddc144b33f8da65e30e53705cfea00d458a2e
Julian <tipler.julian@gmail.com>
2021-04-16 10:33:34 -0700

    add edit functionality to title and text of each note

--- src/components/framework.jsx
+++ src/components/framework.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 import Note from './note'
+import './framework.css'
 
 export default class Framework extends Component {
     constructor(props) {
         super(props)
         this.state = {
             notes:[],
-            currentNote:{},
+            currentNoteIndex: -1,
             formTitle:"",
             formText:""
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.selectNote = this.selectNote.bind(this)
+        this.onUpdateNote = this.onUpdateNote.bind(this)
     }
 
     handleSubmit(e) {
@@ -32,24 +35,39 @@ export default class Framework extends Component {
         this.setState({ [e.target.name] : e.target.value });
     }
 
+    selectNote(e,index) {
+        e.preventDefault();
+        let currentNote = this.state.notes[index];
+        this.setState({currentNoteIndex:index})
+    }
+
+    onUpdateNote(e,index){
+        const updatedNotes = this.state.notes.map((note,i)=> {
+            if(index===i){
+                note[e.target.name] = e.target.value
+            }
+            return note
+        })
+        this.setState({notes:updatedNotes})
+    }
     
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <input name='formTitle' onChange={this.handleChange} value={this.state.formTitle}></input>
-                    <input name='formText' onChange={this.handleChange} value={this.state.formText}></input>
+                    <textarea name='formText' onChange={this.handleChange} value={this.state.formText}></textarea>
                     <button type='submit'>Add Note</button>
                 </form>
-                <div className='note-display'>
-                    {this.state.notes.map((note,i) => {
-                        return <Note key={i} title={note.title} text={note.text}></Note>
-                    })}
-                </div>
-                <div className='notes-titles'>
-                    {this.state.notes.map((note,i) => {
-                        return <div key={i}>{note.title}</div>
-                    })}
+                <div className='main-container'>
+                    <div className='notes-titles'>
+                        {this.state.notes.map((note,i) => {
+                            return <div className={`note-title ${note.index === this.state.currentNoteIndex ? 'selected' : ''}`}onClick={e=>this.selectNote(e,i)} key={i} >{note.title}</div>
+                        })}
+                    </div>
+                    <div className='note-display'>
+                        {this.state.currentNoteIndex === -1 ? <div className='note'></div> : <Note className='note' index={this.state.currentNoteIndex}title={this.state.notes[this.state.currentNoteIndex].title} text={this.state.notes[this.state.currentNoteIndex].text} onUpdateNote={this.onUpdateNote}></Note>}
+                    </div>
                 </div>
             </div>
         )
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -4,12 +4,12 @@ export default class Note extends Component {
     constructor(props) {
         super(props)
     }
-    
+
     render() {
         return (
             <div>
-                <div className='title'>{this.props.title}</div>
-                <div className='text'>{this.props.text}</div>
+                <input name="title" value={this.props.title} onChange={e=> this.props.onUpdateNote(e,this.props.index)} className='title'/>
+                <input name='text' value={this.props.text} onChange={e=>this.props.onUpdateNote(e,this.props.index)} className='text'/>
             </div>
         )
     }
